6a55c3792e7158c15ff42022676ae8f867cffc5f
mimi07 <mimiionescu07@gmail.com>
2022-06-11 00:02:19 +0300

    Start with distro specific files

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,14 @@
 
 distro="$(dmesg | grep "Linux version" | awk {' print $6 '} | sed -e 's/^([a-z]*@//' | sed -e 's/)//')"
 
-echo $distro
+if [ $distro == "archlinux" ]; do
+  pacman -Sy --noconfirm wget unzip
+  wget https://github.com/mimi0000oo/deploylinux/archive/testing.zip
+  deploylinux-testing/./arch.sh
+
+else
+  wget https://github.com/mimi0000oo/deploylinux/archive/testing.zip
+  unzip testing.zip
+  deploylinux-testing/./gentoo.sh
+
+fi
