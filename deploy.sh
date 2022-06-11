7b4acff0c24e10f8bf37d02a9a2b84a7d55e31db
mimi07 <mimiionescu07@gmail.com>
2022-06-11 12:34:11 +0300

    incorrect zip file name

--- deploy.sh
+++ deploy.sh
@@ -5,12 +5,12 @@ distro="$(dmesg | grep "Linux version" | awk {' print $6 '} | sed -e 's/^([a-z]*
 if [ $distro == "archlinux" ]; then
   pacman -Sy --noconfirm wget unzip
   wget https://github.com/mimi0000oo/deploylinux/archive/testing.zip
-  unzip deploylinux-testing.zip
+  unzip testing.zip
   deploylinux-testing/./arch.sh
 
 else
   wget https://github.com/mimi0000oo/deploylinux/archive/testing.zip
-  unzip deploylinux-testing.zip
+  unzip testing.zip
   deploylinux-testing/./gentoo.sh
 
 fi
