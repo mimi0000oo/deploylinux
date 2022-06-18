ad205ca6e42ada7a6399eef2a81603d8c8b13d82
mimi07 <mimiionescu07@gmail.com>
2022-06-18 22:04:44 +0300

    solve test

--- gentoo/create_partitions.sh
+++ gentoo/create_partitions.sh
@@ -10,24 +10,25 @@ check_efi() { ./other/check_efi.sh; }
 if [ "$1" = 2 ]; then # bios mode
 
   select_drive() {
-    printf ${WHITE}"\nThis is your curent configuration:\n${MAGENTA}$(lsblk -o MODEL,NAME,SIZE,TYPE,MOUNTPOINTS,HOTPLUG)"
+    printf ${WHITE}"\nThis is your curent configuration:\n${MAGENTA}$(lsblk -o MODEL,NAME,SIZE,TYPE,MOUNTPOINTS,HOTPLUG)\n"
     option_prompt "Which drive do you want to install Gentoo on?"
     read drive
-    printf ${CYAN}"You selected drive ${YELLOW}\"$drive\""
+    solve_optprompt $drive "printf ${CYAN}"You selected drive ${YELLOW}\"$drive\"\n"" 
         
     select_partition_route() {
 
       option_prompt "What do you want to do from here?" "Go with predefined partitions" "Create your own partitions" "Back" "Quit"
       read partitions
-      predefined_partitions="NAME   SIZE    TYPE    MOUNTPOINTS\n \
-        sda    $(lsblk | grep $drive -m1 | awk {' print $4 '})    disk\n \
-        sda1    256M    EFI System    /boot\n \
-        sda2    4G    Linux Swap    [SWAP]\n \
-        sda3  $(( $(( $(lsblk -b | grep $drive -m1 | awk {' print $4 '}) - (256 * 1048576) - (4 * 1024 * 1048576) )) / 1073741824 ))G    Linux filesystem    /"
+      predefined_partitions="NAME   SIZE    TYPE    MOUNTPOINTS\n\
+        sda    $(lsblk | grep $drive -m1 | awk {' print $4 '})    disk\n\
+        sda1    256M    EFI System    /boot\n\
+        sda2    4G    Linux Swap    [SWAP]\n\
+        sda3  $(( $(( $(lsblk -b | grep $drive -m1 | awk {' print $4 '}) - (256 * 1048576) - (4 * 1024 * 1048576) )) / 1073741824 ))G    Linux filesystem"
           
       solve_optprompt $partitions "echo The predefined partitions are:\n$predefined_partitions"
 
     }
+    
 
     select_partition_route
 
