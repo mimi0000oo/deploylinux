8ccc15ada3fe5463514894b3cc3fa11c05e0731a
mimi07 <mimiionescu07@gmail.com>
2022-06-21 19:54:46 +0300

    get close with creating partitions / 1st test with disk modifications

--- gentoo/create_partitions.sh
+++ gentoo/create_partitions.sh
@@ -2,7 +2,7 @@
 
 # include files
 source ./prompts/option_prompt.sh
-source ./prompts/solve_optprompt.sh
+source .import_configurationts/solve_optprompt.sh
 source ./prompts/yn_prompt.sh
 source ./prompts/solve_ynprompt.sh
 source ./other/colors.sh
@@ -35,44 +35,92 @@ if [ "$1" = 2 ]; then # bios mode
 
       select_partition_route() {
 
-        echo ignore test
-
-        option_prompt "${MAGENTA}What do you want to do from here?" "Go with predefined partitions" "Create your own partitions" "Back" "Quit"
+        option_prompt "What do you want to do from here?" "Go with predefined partitions" "Create your own partitions" "Back" "Quit"
         read partitions
-      
+  
         case $partitions in
           1)
+
             lastdrive_size=$(( $(( $(lsblk -b | grep -m1 $disk | awk {' print $4 '}) - (256 * 1048576) - (4 * 1024 * 1048576) )) / 1073741824 ))
 
-              printf "The predefined partitions are:\n"
-              printf "NAME        SIZE            TYPE                MOUNTPOINTS\n"
-              printf "${disk}         $(lsblk | grep -m1 $disk | awk {' print $4 '})          disk\n"
-              printf "${disk}1        256M            EFI System          /boot\n"
-              printf "${disk}2        4G              Linux Swap          [SWAP]\n"
-              printf "${disk}3        ${lastdrive_size}G            Linux filesystem    /\n"
+            printf "The predefined partitions are:\n"
+            printf "NAME        SIZE            TYPE                MOUNTPOINTS\n"
+            printf "${disk}         $(lsblk | grep -m1 $disk | awk {' print $4 '})          disk\n"
+            printf "${disk}1        256M            EFI System          /boot\n"
+            printf "${disk}2        4G              Linux Swap          [SWAP]\n"
+            printf "${disk}3        ${lastdrive_size}G            Linux filesystem    /\n"
+            
+            option_prompt "Are you sure you want to continue?\n${RED}WARNING${YELLOW}THIS WILL ERAZE ALL THE DATA ON YOUR DISK!!${MAGENTA}" "Continue with this partition scheme" "Make your own partitions" "Back" "Quit"
+            read partitions_confirmation
+            
+            case $partitions_confirmation in 
 
-              yn_prompt "Are you ok with this partitions?"
-              solve_ynprompt "echo test" select_partition_route
+              1)
 
-              ;;
+                #disk
+                wipefs -a /dev/sda
+                #disk1-3
+                (echo o; echo n; echo p; echo ; echo ; echo +256M; echo n; echo p; echo ; echo ; echo +4G; echo t; echo 82; echo n; echo p; echo ; echo; echo; echo w;) | fdisk /dev/$disk
 
-          2)
-            echo to do
-            ;;
+                #filesystems
+                mkfs.vfat -F 32 /dev/${disk}1
+                mkfs.ext4 /dev/${disk}3
+                mkswap /dev/${disk}2
+                swapon /dev/${disk}2
 
-          3) 
-            choose_drive
-            ;;
+                ;;
 
-          4) 
-            printf "Goodbye!\n"
+            esac
+
             ;;
 
         esac
-
       }
 
       select_partition_route
+
+      #select_partition_route() {
+
+      #  option_prompt "${MAGENTA}What do you want to do from here?" "Go with predefined partitions" "Create your own partitions" "Back" "Quit"
+      #  read partitions
+      
+      #  case $partitions in
+      #    1)
+      #      lastdrive_size=$(( $(( $(lsblk -b | grep -m1 $disk | awk {' print $4 '}) - (256 * 1048576) - (4 * 1024 * 1048576) )) / 1073741824 ))
+
+      #        printf "The predefined partitions are:\n"
+      #        printf "NAME        SIZE            TYPE                MOUNTPOINTS\n"
+      #        printf "${disk}         $(lsblk | grep -m1 $disk | awk {' print $4 '})          disk\n"
+      #        printf "${disk}1        256M            EFI System          /boot\n"
+      #        printf "${disk}2        4G              Linux Swap          [SWAP]\n"
+      #        printf "${disk}3        ${lastdrive_size}G            Linux filesystem    /\n"
+
+      #        yn_prompt "Are you ok with this partitions?"
+      #        solve_ynprompt "echo test" select_partition_route
+
+      #        ;;
+
+      #    2)
+      #      echo to do
+      #      ;;
+
+      #    3) 
+      #      choose_drive
+      #      ;;
+
+      #    4) 
+      #      printf "Goodbye!\n"
+      #      ;;
+
+      #    *)
+      #      select_partition_route
+      #      printf "${YELLOW}\"$1\"${RED} is not a valid option!${WHITE}\n"
+      #      ;;
+      #  esac
+
+      #}
+
+      #select_partition_route
 
     }
 
--- prompts/option_prompt.sh
+++ prompts/option_prompt.sh
@@ -6,16 +6,19 @@ source ./other/distro.sh
 # include end
 
 option_prompt() {
-  printf "$1\n"
   args=("$@")
 
   if [[ $distro == "archlinux" ]]; then
+    printf $BLUE"$1\n"
+    
     for (( i=1; i<=$(($# - 1)); i++ )); do
       printf "${BLUE}$i)$WHITE ${args[${i}]}\n$BLUE"
     done
     printf "$BLUE> $WHITE"
 
   else 
+    printf $MAGENTA"$1\n"
+
     for (( i=1; i<=$(($# - 1)); i++ )); do
       printf "${MAGENTA}$i)$WHITE ${args[${i}]}\n$MAGENTA"
     done
